151ca6a0108447b125582b7ef485af3272ce0782
Siddharth Nahar <siddharthnahar68@gmail.com>
2024-03-04 18:02:42 +0530

    Ui of Signup and login page with Databse setup is completed

--- lib/app/core/routes/app_routes.dart
+++ lib/app/core/routes/app_routes.dart
@@ -1,22 +1,30 @@
 // routes.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:task_innovatrix_infotech/app/modules/home/view/home_screen.dart';
 import 'package:task_innovatrix_infotech/app/modules/login/view/login_screen.dart';
+import 'package:task_innovatrix_infotech/app/modules/signup/view/signup_view.dart';
 import 'package:task_innovatrix_infotech/app/modules/splash/view/splash_screen.dart';
 
 final routerProvider = Provider((ref) => Routes());
 
 class Routes {
   static const String splash = '/';
-  static const String home = '/home';
   static const String login = '/login';
+  static const String signup = '/signup';
+  static const String home = '/home';
 
   Route<dynamic>? generateRoute(RouteSettings settings) {
+    print("The settings is ${settings.name}");
     switch (settings.name) {
       case splash:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case login:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case signup:
+        return MaterialPageRoute(builder: (_) => const SignUpScreen());
+      case home:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
 
       default:
         return MaterialPageRoute(builder: (_) => const UnknownScreen());
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:task_innovatrix_infotech/app/core/navigation/navigation_service.dart';
 import 'package:task_innovatrix_infotech/app/core/routes/app_routes.dart';
+import 'package:task_innovatrix_infotech/app/data/db/db_helper.dart';
 
 final navigatorProvider = Provider((ref) => NavigatorService());
+late DatabaseHelper db;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final db = DatabaseHelper();
+  await db.initDB();
   runApp(const ProviderScope(child: MyApp()));
 }
 
